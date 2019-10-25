a4377da51d7a1c14a73adf970c661e96f11dd142
Maxim Rakhmanin <rakhmaxdev@gmail.com>
2019-10-25 17:40:41 +0300

    Clean up code

--- Lab_eToken.cpp
+++ Lab_eToken.cpp
@@ -1,10 +1,6 @@
 #include "Lab_eToken.h"
 #include <Windows.h>
 #include "lib/cryptoki.h"
-//#include "lib/otp-pkcs11.h"
-//#include "lib/eTSAPI.h"
-//#include "lib/pkcs11.h"
-//#include "lib/pkcs11t.h"
 #include "lib/eTPkcs11.h"
 
 using namespace labetoken;
@@ -12,8 +8,6 @@ using namespace labetoken;
 [STAThreadAttribute]
 int main(array<System::String^>^ args)
 {
-
-
 	FreeConsole();
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
